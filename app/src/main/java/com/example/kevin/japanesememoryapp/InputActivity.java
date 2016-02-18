649be1692df8f2677ee81bf550ffb471df93565b
Kevin van der Vleuten <kevin.vd.vleuten@gmail.com>
2016-02-18 10:22:49 +0100

    added check for existance before kanji is added

--- app/src/main/java/com/example/kevin/japanesememoryapp/InputActivity.java
+++ app/src/main/java/com/example/kevin/japanesememoryapp/InputActivity.java
@@ -4,6 +4,7 @@ import android.content.ContentValues;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.renderscript.ScriptGroup;
 import android.support.v7.app.AlertDialog;
@@ -15,6 +16,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class InputActivity extends AppCompatActivity {
     public static int DEFAULT_DIFFICULTY = 5;
@@ -49,7 +52,26 @@ public class InputActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if(verifyInput()) {
-                    saveKanji();
+                    if(checkKanjiExists(tbx_kanji.getText().toString())) {
+                        DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                switch (which) {
+                                    case DialogInterface.BUTTON_POSITIVE:
+                                        saveKanji();
+                                        break;
+                                    case DialogInterface.BUTTON_NEGATIVE:
+                                        //Do nothing, close dialog
+                                        break;
+                                }
+                            }
+                        };
+                        AlertDialog.Builder builder = new AlertDialog.Builder(InputActivity.this);
+                        builder.setMessage(getString(R.string.inputKanjiExists) + tbx_kanji.getText().toString() + getString(R.string.inputAddAnyway)).setPositiveButton(getString(R.string.yes), dialogClickListener).setNegativeButton(getString(R.string.no), dialogClickListener).show();
+                    }
+                    else {
+                        saveKanji();
+                    }
                 }
                 else {
                     MainActivity.showAlert(InputActivity.this, getString(R.string.error), getString(R.string.inputFillInAllPlease));
@@ -168,6 +190,28 @@ public class InputActivity extends AppCompatActivity {
         else {
             MainActivity.showAlert(InputActivity.this, getString(R.string.error), getString(R.string.inputUpdateWrong));
         }
+    }
+
+    private boolean checkKanjiExists(String input) {
+        dbHelper = new FeedReaderDbHelper(InputActivity.this);
+
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+
+        String[] selectQuery = {
+                FeedReaderContract.FeedEntry.COLUMN_NAME_KANJI
+        };
+
+        Cursor cursor = db.query(
+                FeedReaderContract.FeedEntry.TABLE_NAME,
+                selectQuery,
+                FeedReaderContract.FeedEntry.COLUMN_NAME_KANJI + "=?",
+                new String[]{input},
+                null,
+                null,
+                ""
+        );
+
+        return (cursor.getCount() > 0);
     }
 
     private boolean verifyInput() {
